9720625aa49825afb94d8cfee3bda4b5189e905f
Ijallcmd <101429772+Ijallcmd@users.noreply.github.com>
2022-07-09 22:01:54 +0700

    install.sh

---
+++ install.sh
@@ -0,0 +1,60 @@
+#! /usr/bin/bash
+null="> /dev/null 2>&1"
+g="\033[1;32m"
+r="\033[1;31m"
+b="\033[1;34m"
+w="\033[0m"
+mkdir -p $HOME/.config/clash
+echo ""
+
+echo -e $b"➤ "$w"Clash For Termux Non Root"
+echo ""
+
+echo -e $b"➤ "$w" prepare for installing dependencies ..."
+echo ""
+
+echo -e $b"➤ "$w" installing pacakge: "$g"clash"$w
+pkg install clash
+echo ""
+
+echo -e $b"➤ "$w" installing pacakge: "$g"unzip"$w
+pkg install unzip
+echo ""
+
+echo -e $b"➤ "$w" installing pacakge: "$g"nano"$w
+pkg install nano
+echo ""
+
+cd $HOME/clashtermux
+sleep 2
+
+echo -e $b"➤ "$w" coppying yacd-dashbord: "$g"yacd"$w
+cp -r -f yacd-gh-pages.zip $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying master-isp.yaml: "$g"master-isp"$w
+cp -r -f master-isp.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+echo -e $b"➤ "$w" coppying config.yaml: "$g"config"$w
+cp -r -f config.yaml $HOME/.config/clash
+echo ""
+
+sleep 2
+
+cd $HOME/.config/clash
+echo -e $b"➤ "$w" extracting yacd-dashbord: "$g"yacd"$w
+unzip yacd-gh-pages.zip
+echo ""
+
+sleep 2
+rm -r -f yacd-gh-pages.zip
+sleep 2
+cd
+rm -r -f clashtermux
+clash
+clear
